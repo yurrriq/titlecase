8fb32ee4512db20996293d86ee256e3d455d9510
Eric Bailey <eric@ericb.me>
2017-03-07 06:47:50 -0600

    Add --help and support plaintext
    This is pretty quick and dirty, but it gets the job done.
    
    With `--format=plain` you now get, e.g. `'` instead of `&#39`.

--- Main.hs
+++ Main.hs
@@ -1,10 +1,56 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main ( main ) where
 
-import           Data.Text                  (pack)
-import           Data.Text.Titlecase
-import           System.Environment
-import           Text.Blaze
-import           Text.Blaze.Renderer.Pretty
+import           Prelude                      hiding (unwords)
+
+import           Control.Arrow                (second, (&&&), (<<<))
+
+import           Data.Text                    (Text, unpack, unwords)
+import           Data.Text.Titlecase          (Titlecase, titlecase)
+import           Data.Text.Titlecase.Internal (unTitlecase)
+
+import           System.Console.CmdArgs
+
+import           Text.Blaze                   (toMarkup)
+import           Text.Blaze.Renderer.Pretty   (renderMarkup)
+
+data Format = HTML
+            | Plain
+            deriving (Data, Show)
+
+data Options = Options { format :: Format
+                       , input  :: [Text]
+                       }
+             deriving (Data, Show, Typeable)
 
 main :: IO ()
-main = getArgs >>= putStr . renderMarkup . toMarkup . titlecase . pack . unwords
+main = putStr . go =<< cmdArgs options
+  where
+    go = uncurry render <<<
+         second (titlecase . unwords) <<<
+         format &&& input
+
+render :: Format -> Titlecase -> String
+render HTML  = renderMarkup . toMarkup
+render Plain = (++ "\n") . unpack . unTitlecase
+
+options :: Options
+options = Options
+  { format = HTML &=
+    typ "FORMAT" &=
+    help "html (default) or plain"
+  , input = def &=
+    typ "WORDS" &=
+    args
+  } &=
+  help "Convert English words to title case" &=
+  helpArg [explicit, name "h", name "help"] &=
+  summary "titlecase v0.1.1.3" &=
+  details [ "Capitalize all English words except articles (a, an, the),"
+          , "coordinating conjunctions (for, and, nor, but, or, yet, so),"
+          , "and prepositions (unless they begin or end the title)."
+          , ""
+          , "The prepositions are taken from"
+          , "<https://en.wikipedia.org/wiki/List_of_English_prepositions>."
+          ]
